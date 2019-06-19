1888d271c893a83a3a1fbf41d99f3f767de1a086
Sonia Meller <mellersonia@gmail.com>
2019-06-19 15:23:57 +0100

    Fix for GitHub

--- js/Background.js
+++ js/Background.js
@@ -1,7 +1,7 @@
 class Background {
   constructor() {
     this.img = new Image();
-    this.img.src = "../img/Background.png";
+    this.img.src = "img/Background.png";
   }
   draw(ctx) {
     ctx.drawImage(this.img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
